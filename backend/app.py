4cd7db373d49d2ad22a7f498ba471ac197d5e09d
Mateusz Kruk <krukm634@gmail.com>
2024-03-28 21:02:35 +0100

    feat: commands from backend

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
@@ -25,6 +26,7 @@ class Config(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     value = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
 
 
 admin.add_view(ModelView(Config, db.session))
@@ -42,16 +44,28 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    # howMuchTimeToConvention = Config(name='ile_do_futrolajek', value='621')
-    # conventionDate = Config(name='data', value='6.2.1')
-    # discordLink = Config(name='discord', value='abc')
-    # websiteLink = Config(name='website', value='def')
+    # howMuchTimeToConvention = Config(name='ile_do_futrolajek', value='621', description='ile dni jest do futrołajek')
+    # conventionDate = Config(name='data', value='6.2.1', description='ważne daty')
+    # discordLink = Config(name='discord', value='abc', description='link do discorda')
+    # websiteLink = Config(name='website', value='def', description='link do strony')
     # db.session.add(howMuchTimeToConvention)
     # db.session.add(conventionDate)
     # db.session.add(discordLink)
     # db.session.add(websiteLink)
     # db.session.commit()
 
+@app.route('/configs')
+def get_all_users():
+    result = db.session.query(Config).all()
+    if result is not None:
+        return json.dumps({
+            'result': [{
+                'name': config.name,
+                'text': config.value,
+                'description': config.description
+            } for config in result]
+        })
+    return "No informations yet"
 
 @app.route('/config/<name>')
 def get_users(name):
@@ -59,7 +73,6 @@ def get_users(name):
     if result is not None:
         return result.value
     return "No informations yet"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ bot/integrations/ReniaBackofficeClient.py
@@ -0,0 +1,23 @@
+
+from typing import List
+import requests
+import json
+
+from commands import BaseCommand, ManagedCommand
+from logs import logger
+
+
+class ReniaBackofficeClient:
+    url = 'http://renia-tg-backend:5000'
+
+    def get_commands(self) -> List[BaseCommand]:
+        res = requests.get(f'{self.url}/configs')
+
+        if res.status_code != 200:
+            raise requests.RequestException(f'Backoffice odpowiedział kodem {res.status_code} ({res.reason})')
+        
+        commands = [ManagedCommand(**data) for data in json.loads(res.text)['result']]
+
+        logger.info('Komendy zostały wczytane!')
+
+        return commands
