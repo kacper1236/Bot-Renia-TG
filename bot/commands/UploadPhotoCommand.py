1d7adcef6790952b86b485250c566b4df09a99ee
kacper1236 <kacper2486@gmail.com>
2024-03-29 17:54:42 +0100

    Edit UploadPhotoCommand
    Trzeba poprawić, bo albo nie rozumiem kompletnie komend albo coś jest nie tak

--- bot/commands/UploadPhotoCommand.py
+++ bot/commands/UploadPhotoCommand.py
@@ -1,20 +1,19 @@
-from telegram import Update
+from telegram import Update, Message
 from telegram.ext import CallbackContext
 from . import BaseCommand, command_with_logs, CommandManager
+
+from datetime import datetime, timezone
 
 class UploadPhotoCommand(BaseCommand):
     name = "zdjecia"
     description = "Wyślij zdjęcia"
 
-    def __init__(self, manager: CommandManager) -> None:
-        self.__manager = manager
-
     @command_with_logs
-    async def callback(self, update: Update, context: CallbackContext):
+    async def callback(self, update: Update, context: Message):
+        await update.message.reply_text("Siema")
         user_id = update.message.from_user.id
-        timestamp = update.message.date.strftime("%Y%m%d%H%M%S")
-        file_id = update.message.photo[-1].file_id
-        file_path = context.bot.get_file(file_id).file_path
-        download_path = f"C:\\Users\\Rogal\\Documents\\GitHub\\Bot-Renia-TG\\photos\\{user_id}_{timestamp}.jpg"
-        context.bot.download_file(file_path, download_path)
+        timestamp = datetime.now().timestamp()
+        new_file = await context.effective_attachment[-1].get_file()
+        print(new_file)
+        await new_file.download_to_drive(custom_path=f"C:\\Users\\kacpe\\Documents\\GitHub\\Bot-Renia-TG\\photos\\{user_id}_{timestamp}.jpg")
         await update.message.reply_text("Zdjęcie zostało zapisane")
