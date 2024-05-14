4643d942303e4dfc37a9d3ab7c6e52b8a7ec3776
Damian Chocianowicz <daronfox95@gmail.com>
2024-05-14 22:25:49 +0200

    /ileDoFutrolajek
    /ileDoFutrolajek

---
+++ bot/commands/HowMuchTimeLeftCommand.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler
+from . import command_with_logs, BaseCommand
+import requests
+import datetime
+
+class HowMuchTimeLeftCommand(BaseCommand):
+    name = 'ileDoFutrolajek'
+
+    @command_with_logs
+    async def callback(self, update: Update, context: CallbackContext):
+        print("before request")
+        event_date_str = requests.get('https://futrolajki.pl/app/event/info').json()['eventDate']
+        event_date = datetime.datetime.strptime(event_date_str, "%Y-%m-%d").date()
+        days_left = (event_date - datetime.date.today()).days
+        await update.message.reply_text(f'Do Futrołajek zostało {days_left} dni')
