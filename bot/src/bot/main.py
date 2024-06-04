fcd5a6537f0c5d96b753e421e189c36976f91129
damianc <damian.chocianowicz@gmail.com>
2024-06-04 21:40:12 +0200

    Merging fix

--- bot/src/bot/main.py
+++ bot/src/bot/main.py
@@ -3,14 +3,15 @@ from dotenv import load_dotenv
 import os
 import sys
 from ..integrations import ReniaBackendClient
-from ..commands import CommandManager, HelpCommand, UploadPhotoCommand, HowMuchTimeLeftCommand
+from ..commands import CommandManager, HelpCommand, UploadPhotoCommand, HowMuchTimeLeftCommand, DatabasePersistence
 from .logs import logger, error
 
 
 def main():
     try:
         load_dotenv()
-        app = ApplicationBuilder().token(os.environ.get('TG_TOKEN')).build()
+        persistence = DatabasePersistence()
+        app = ApplicationBuilder().token(os.environ.get('TG_TOKEN')).persistence(persistence).build()
 
         manager = CommandManager(app)
         available_commands = [
@@ -23,16 +24,15 @@ def main():
 
         manager.setup(available_commands)
         app.add_error_handler(error)
-
         logger.info("Renia jest włączona")
-     
-        app.run_polling() # wątek blokuje się na tym
+        app.run_polling()  # wątek blokuje się na tym
     except KeyboardInterrupt:
         sys.exit(1)
     except Exception:
         logger.exception("Renia napotkała błąd podczas pracy!")
     finally:
         logger.info("Renia konczy działanie")
+
 
 if __name__ == "__main__":
     main()
--- bot/src/commands/HelpCommand.py
+++ bot/src/commands/HelpCommand.py
@@ -1,7 +1,7 @@
-
 from telegram import Update
 from telegram.ext import CallbackContext
 from . import SlashCommand, command_with_logs, CommandManager
+
 
 class HelpCommand(SlashCommand):
     name = 'help'
